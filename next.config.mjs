2f4a2dee88b3fc39a4a0a589f0edb111d7637646
Yordan <yordanpz@hotmail.com>
2024-03-17 01:03:50 -0300

    Add reactStrictMode, images domains, and async headers to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['res.cloudinary.com','images.unsplash.com'],
+    },
+    async headers() {
+        return [
+        {
+            source: '/(.*)',
+            headers: [
+            {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+            },
+            ],
+        },
+        ];
+    },
+};
 
 export default nextConfig;
